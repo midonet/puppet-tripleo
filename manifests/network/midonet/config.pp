c51cf394440c71eb100ec512a342fdaef27976a4
Alejandro Andreu <alejandroandreu@openmailbox.org>
2016-11-23 17:31:35 +0100

    Adds new tripleo::network::midonet::config class
    This class only creates an initial set of networks plus the edge router
    that is necessary by MidoNet to operate correctly. This could be done
    manually but then the gateway class wouldn't work.
    
    Change-Id: Ie1146da97f5e1f85c9af0123367b488bcc1da7b4
    Partial-Bug: #1647302

---
+++ manifests/network/midonet/config.pp
@@ -0,0 +1,102 @@
+#
+# Copyright (C) 2016 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: tripleo::network::midonet::config
+#
+# Configure Neutron properly and create the edge router as well as the
+# initial networks
+#
+# == Parameters:
+#
+# [*step*]
+#   (Optional) The current step in deployment. See tripleo-heat-templates
+#   for more details.
+#   Defaults to hiera('step')
+#
+# [*edge_router_name*]
+#   (Optional) Name that will be given to the edge router.
+#   Defaults to hiera('nc_edge_router_name', undef)
+#
+# [*edge_network_name*]
+#   (Optional) Name of the external network where the port will be bound.
+#   Defaults to hiera('nc_edge_network_name', undef)
+#
+# [*edge_subnet_name*]
+#   (Optional) Name of the subnet where the port will be bound.
+#   Defaults to hiera('nc_edge_subnet_name', undef)
+#
+# [*edge_cidr*]
+#   (Optional) CIDR of this network.
+#   Defaults to hiera('nc_edge_cidr', undef)
+#
+# [*port_name*]
+#   (Optional) Name of the neutron port.
+#   Defaults to hiera('nc_port_name', undef)
+#
+# [*port_fixed_ip*]
+#   (Optional) IP that the bound interface has.
+#   Defaults to hiera('nc_port_fixed_ip', undef)
+#
+# [*port_interface_name*]
+#   (Optional) Name of the bound interface.
+#   Defaults to hiera('nc_port_interface_name', undef)
+#
+# [*gateway_ip*]
+#   (Optional) IP of the edge router in the floating IP network.
+#   Defaults to hiera('nc_gateway_ip', undef)
+#
+# [*subnet_cidr*]
+#   (Optional) CIDR belonging to the floating IP network.
+#   Defaults to hiera('nc_subnet_cidr', undef)
+#
+# [*allocation_pools*]
+#   (Optional) Allocation pool on the FIP network.
+#   Defaults to hiera('nc_allocation_pools', undef)
+#
+# [*neutron_tenant_name*]
+#   (Optional) Tenant name that is being used with Neutron.
+#   Defaults to hiera('neutron_auth_tenant', undef)
+#
+class tripleo::network::midonet::config(
+  $edge_router_name      = hiera('nc_edge_router_name', undef),
+  $edge_network_name     = hiera('nc_edge_network_name', undef),
+  $edge_subnet_name      = hiera('nc_edge_subnet_name', undef),
+  $edge_cidr             = hiera('nc_edge_cidr', undef),
+  $port_name             = hiera('nc_port_name', undef),
+  $port_fixed_ip         = hiera('nc_port_fixed_ip', undef),
+  $port_interface_name   = hiera('nc_port_interface_name', undef),
+  $gateway_ip            = hiera('nc_gateway_ip', undef),
+  $subnet_cidr           = hiera('nc_subnet_cidr', undef),
+  $allocation_pools      = hiera('nc_allocation_pools', undef),
+  $neutron_tenant_name   = hiera('neutron_auth_tenant', undef),
+  $step                  = hiera('step'),
+) {
+  if $step >= 4 {
+    midonet::resources::network_creation { 'Edge Router Setup':
+      tenant_name         => $neutron_tenant_name,
+      edge_router_name    => $edge_router_name,
+      edge_network_name   => $edge_network_name,
+      edge_subnet_name    => $edge_subnet_name,
+      edge_cidr           => $edge_cidr,
+      port_name           => $port_name,
+      port_fixed_ip       => $port_fixed_ip,
+      port_interface_name => $port_interface_name,
+      gateway_ip          => $gateway_ip,
+      subnet_cidr         => $subnet_cidr,
+      allocation_pools    => [$allocation_pools],
+      require             => Class['::neutron'],
+    }
+  }
+}
---
+++ spec/classes/tripleo_midonet_config_spec.rb
@@ -0,0 +1,72 @@
+#
+# Copyright (C) 2016 Midokura SARL
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+require 'spec_helper'
+
+describe 'tripleo::network::midonet::config' do
+
+  shared_examples_for 'tripleo::network::midonet::config' do
+
+    context 'with step 2' do
+      let :params do
+        {
+          :step => 2,
+        }
+      end
+
+      it 'should not create a set of initial networks' do
+        is_expected.to_not contain_midonet__resources__network_creation('Edge Router Setup')
+      end
+    end
+
+    context 'with step 4' do
+      let :params do
+        {
+          :step                => 4,
+          :port_interface_name => 'enp0s3',
+          :neutron_tenant_name => 'my_tenant',
+          :edge_router_name    => 'my_edge_router',
+          :edge_network_name   => 'my_edge_network',
+          :edge_subnet_name    => 'my_edge_subnet',
+          :port_name           => 'my_port',
+        }
+      end
+
+      before(:each) do
+        Puppet::Parser::Functions.newfunction(:c7_int_name, :type => :rvalue) { |args|
+          raise ArgumentError, 'expected modern interface name' unless args[0] == 'enp0s3'
+          'eth0'
+        }
+      end
+
+      it 'should create a set of initial networks' do
+        is_expected.to contain_midonet__resources__network_creation('Edge Router Setup')
+      end
+    end
+  end
+
+  on_supported_os.each do |os, facts|
+    context "on #{os}" do
+      let(:facts) do
+        facts.merge({
+          :hostname => 'node.example.com',
+          :fqdn     => 'node.example.com',
+        })
+      end
+
+      it_behaves_like 'tripleo::network::midonet::config'
+    end
+  end
+end
