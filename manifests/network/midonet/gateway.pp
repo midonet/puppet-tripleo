510122d4ff6f1b000a0686728237ccb0aa9a46b7
Alejandro Andreu <alejandroandreu@openmailbox.org>
2017-02-03 11:01:35 +0100

    Update

--- manifests/network/midonet/gateway.pp
+++ manifests/network/midonet/gateway.pp
@@ -157,7 +157,7 @@ class tripleo::network::midonet::gateway(
   $tenant                  = hiera('auth_tenant', 'admin'),
   $step                    = hiera('step'),
 ) {
-  if $step >= 6 {
+  if $step >= 5 {
 
     if $uplink_type == 'static' {
       class { '::midonet::gateway::static':
